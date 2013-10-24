ba634a45bd3286c3ffd3dfdd9627d3e2f5cf2e7f
Your Name <you@example.com>
2013-10-25 08:30:09 +0900

    第5章 5.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
-TestApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help" 
-  get "static_pages/about" 
-  get "static_pages/contact" 
-#  match '/help',    to: 'static_pages#help',    via: 'get'
-#  match '/about',   to: 'static_pages#about',   via: 'get'
-#  match '/contact', to: 'static_pages#contact', via: 'get'
-#  match '/home'    , to: 'static_pages#home', via: 'get'
+TestApp::Application.routes.draw do 
+  root  'static_pages#home'
+
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/home'    , to: 'static_pages#home', via: 'get'
   resources :microposts
 
   resources :users
